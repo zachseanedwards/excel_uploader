09712828bde47ae29ffcfb41b7f8aa35fc776ebe
zachseanedwards <102534342+zachseanedwards@users.noreply.github.com>
2023-10-11 16:06:29 +0100

    Updated file with temporary upload and download - moved hard_coded version to its own branch

--- app.py
+++ app.py
@@ -1,37 +1,44 @@
 from flask import Flask, request, render_template, send_file
 import pandas as pd
 from datetime import datetime
+import tempfile
 import os
+import io
 
 app = Flask(__name__)
-
-# Define the hardcoded paths for uploads and downloads directories
-UPLOADS_DIR = 'C:\\Users\\zachs\\hello\\.venv\\file_uploader\\uploads'
-DOWNLOADS_DIR = 'C:\\Users\\zachs\\hello\\.venv\\file_uploader\\downloads'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
-            # Generate a unique filename with the current date and time
-            current_time = datetime.now().strftime('%Y-%m-%d_%H-%M')
-            filename = f"{current_time}_{uploaded_file.filename}"
-            file_path = os.path.join(UPLOADS_DIR, filename)
-            uploaded_file.save(file_path)
+            # Create a temporary directory
+            temp_dir = tempfile.TemporaryDirectory()
 
-            # Read the uploaded Excel file into a Pandas DataFrame
-            df = pd.read_excel(file_path)
+            try:
+                # Save the uploaded file to the temporary directory
+                current_time = datetime.now().strftime('%Y-%m-%d_%H-%M')
+                filename = f"{current_time}_{uploaded_file.filename}"
+                file_path = os.path.join(temp_dir.name, filename)
+                uploaded_file.save(file_path)
 
-            # Add 'output1' and 'output2' columns
-            df['output1'] = df['Input'] * 10
-            df['output2'] = df['Input'].astype(str) + 'a'
+                # Read the uploaded Excel file into a Pandas DataFrame
+                df = pd.read_excel(file_path)
 
-            # Save the modified DataFrame as a new Excel file in the downloads directory
-            output_path = os.path.join(DOWNLOADS_DIR, filename)
-            df.to_excel(output_path, index=False, engine='openpyxl')
+                # Add 'output1' and 'output2' columns
+                df['output1'] = df['Input'] * 10
+                df['output2'] = df['Input'].astype(str) + 'a'
 
-            return send_file(output_path, as_attachment=True, download_name=filename)
+                # Convert the DataFrame to an Excel file in memory
+                output_io = io.BytesIO()
+                df.to_excel(output_io, index=False, engine='openpyxl')
+
+                # Return the file as a response for download
+                output_io.seek(0)
+                return send_file(output_io, as_attachment=True, download_name=filename)
+            finally:
+                # Clean up the temporary directory
+                temp_dir.cleanup()
 
     return render_template('index.html')
 
