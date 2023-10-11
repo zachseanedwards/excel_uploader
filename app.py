e11b6ee04278ed785834582a07d9fb3265e4d5e1
zachseanedwards <102534342+zachseanedwards@users.noreply.github.com>
2023-10-11 16:04:50 +0100

    Hard coded loading of data
    upd

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ import os
 
 app = Flask(__name__)
 
+# Define the hardcoded paths for uploads and downloads directories
+UPLOADS_DIR = 'C:\\Users\\zachs\\hello\\.venv\\file_uploader\\uploads'
+DOWNLOADS_DIR = 'C:\\Users\\zachs\\hello\\.venv\\file_uploader\\downloads'
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
-            # Generate a unique filename with current date and time
+            # Generate a unique filename with the current date and time
             current_time = datetime.now().strftime('%Y-%m-%d_%H-%M')
             filename = f"{current_time}_{uploaded_file.filename}"
-            file_path = os.path.join('uploads', filename)
+            file_path = os.path.join(UPLOADS_DIR, filename)
             uploaded_file.save(file_path)
 
             # Read the uploaded Excel file into a Pandas DataFrame
@@ -23,8 +27,8 @@ def index():
             df['output1'] = df['Input'] * 10
             df['output2'] = df['Input'].astype(str) + 'a'
 
-            # Save the modified DataFrame as a new Excel file
-            output_path = os.path.join('downloads', filename)
+            # Save the modified DataFrame as a new Excel file in the downloads directory
+            output_path = os.path.join(DOWNLOADS_DIR, filename)
             df.to_excel(output_path, index=False, engine='openpyxl')
 
             return send_file(output_path, as_attachment=True, download_name=filename)
